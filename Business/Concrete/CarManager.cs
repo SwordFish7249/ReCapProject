47d83f43fd854e8b713fe353c9f3c522445cca44
CEM G <g.raing8@hotmail.com>
2021-02-22 01:27:19 +0200

    Modified ReCap Project for lecture 12 homework1

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,17 +23,16 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.CarName.Length > 2 && car.UnitPrice > 0) //dailyprice olsa daha iyi bu
-            {
+            
+
+            
+
                 _carDal.Add(car);
                 return new SuccessResult(Messages.CarAdded);
-            }
-            else
-            {
-                return new ErrorResult(Messages.CarNameInvalid);
-            }
+                        
         }
 
         public IDataResult<List<Car>> GetAll()
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.CarName).NotEmpty();
+            RuleFor(c=>c.CarName).MinimumLength(2);
+            RuleFor(c => c.UnitPrice).NotEmpty();
+            RuleFor(c => c.UnitPrice).GreaterThan(0);
+            RuleFor(c => c.UnitPrice).GreaterThanOrEqualTo(10).When(c => c.Id == 1);
+        }
+    }
+}
